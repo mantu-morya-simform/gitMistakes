e7f7e965d9db7fe640ca244a3ca740905ae26994
mantu-morya-simform <mantu.morya@simformsolutions.com>
2026-02-16 15:11:22 +0530

    Add correct fav color prompt

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+    char color[50];
+
     printf("Hello, world!\n");
+
+    printf("What's your favorite color? ");
+    fgets(color, sizeof(color), stdin);
+
+    // Remove the newline character, if present
+    color[strcspn(color, "\n")] = '\0';
+
+    printf("Wow, I like %s too!\n", color);
     return 0;
 }
