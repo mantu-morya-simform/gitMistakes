37b7e28ddd1ca9cec95b39515cb53a31809acd81
mantu-morya-simform <mantu.morya@simformsolutions.com>
2026-02-16 15:07:07 +0530

    Ask user for fav color

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
+    char color[50];
+
     printf("Hello, world!\n");
+
+    printf("What's your favorite color? ");
+    fgets(color, sizeof(color), stdin);
+
+    printf("Wow, I like %s too!\n", color);
     return 0;
 }
